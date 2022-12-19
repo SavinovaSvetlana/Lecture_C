4d5f151651067d0d7aa518b4ed64a605869c06df
svetlana <masterica75@mail.ru>
2022-12-19 15:46:51 +0700

    Added Task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -11,10 +11,9 @@ int[] array = { 10, 23, 31, 14, 51, 62, 71, 81, 19 };
 // array[0] = 12;
 // Console.WriteLine(array[4]);
 
-int result = Max(Max(array[0]), Max(array[1]), Max(array[2]), Max(array[3]), Max(array[4]), Max(array[5]),
-Max(array[6]), Max(array[7]), Max(array[8]));
+int result = Max(Max(array[0], array[1], array[2]),
+                 Max(array[3], array[4], array[5]),
+                 Max(array[6], array[7], array[8]));
 Console.WriteLine(result);
 
-
-//НЕ РАБОТАЕТ!!!!!!!!!!!!!
 
---
+++ Task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 10, 23, 31, 14, 51, 62, 71, 81, 19 };
+
+int n = array.Length;
+int find = 14;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+
+    index++;
+}
