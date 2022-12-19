a27b25291cdae5623fc66099e3b38c2cbcd68f21
svetlana <masterica75@mail.ru>
2022-12-19 18:41:06 +0700

    Added Task 7

---
+++ Task_7/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4(int count, string c)
+{
+    string result = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        result = result + c;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
