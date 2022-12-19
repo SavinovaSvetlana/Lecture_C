31a3488ffd00cdcd4e768a59f2ca25babb5b98a8
svetlana <masterica75@mail.ru>
2022-12-19 17:09:58 +0700

    Added Task 4,5

---
+++ Task_4/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection)
+{
+    int Lenght = collection.Length;
+    int index = 0;
+    while (index < Lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10]; // 1      new int[1] - создать новый массив где будет 10 элементов
+FillArray(array);          // 2
+PrintArray(array);
+Console.WriteLine();       // 3
+
+//Задача: опеределили массив из 10 элементов(1), вызвали метод(2), кторый заполнил массив случайными числами,
+//и метод (3), кторый распечатал массив
---
+++ Task_5/Program.cs
@@ -0,0 +1,47 @@
+﻿void FillArray(int[] collection)
+{
+    int Lenght = collection.Length;
+    int index = 0;
+    while (index < Lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;     // Если нет элемнта будет показывать -1
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);     // Ищем в массиве цифру 4, номер элемента 
+Console.WriteLine(pos);
