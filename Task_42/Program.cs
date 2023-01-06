0ceb98f9d6ba7c9e75a0f63e55169373a909a54c
svetlana <masterica75@mail.ru>
2023-01-06 19:05:52 +0700

    Added Task 42

---
+++ Task_42/Program.cs
@@ -0,0 +1,17 @@
+﻿// int Faktorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Faktorial(n - 1);
+// }
+// Console.WriteLine(Faktorial(3));
+
+
+double Faktorial(int n)
+{
+    if (n == 1) return 1;
+    else return n * Faktorial(n - 1);
+}
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Faktorial(i)}");
+}
